20bc108bae9a1ad5adcb0e8153c03b999deb6e8d
jetforce <jetvirtusio@hotmail.com>
2016-04-09 13:52:06 +0800

    idk

--- src/advandb_concurrency/Node.java
+++ src/advandb_concurrency/Node.java
@@ -49,7 +49,7 @@ public class Node {
      
     public void activate() throws IOException{
        
-    
+        
         this.server.start();
         Connector c = new Connector("localhost", this.port ,this.name,"main",this.middle);
         Connector c2 = new Connector("192.168.1.17",1234,this.name,"main",this.middle);
